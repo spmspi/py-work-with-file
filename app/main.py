db7997d4f007edec5e5751a2d3f0fe56aa4c84f8
SPMSPI <kislicin.artur@gmail.com>
2026-03-13 06:47:13 +0200

    ready

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    one_file = open(data_file_name, "r")
+    supply = 0
+    buy = 0
+    for line in one_file.readlines():
+        list_str = line.split(",")
+        if list_str[0] == "supply":
+            supply += int(list_str[1])
+        if list_str[0] == "buy":
+            buy += int(list_str[1])
+    one_file.close()
+    result = supply - buy
+    report = open(report_file_name, "w")
+    report.write(f"supply,{supply}\n")
+    report.write(f"buy,{buy}\n")
+    report.write(f"result,{result}\n")
+    report.close()
